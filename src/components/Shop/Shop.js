3b7a5f7fb8884f345f373a1f713e29ddd2db9a48
Fahim Rimon <fahimrimon21@gmail.com>
2022-03-25 12:54:12 -0700

    img show

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css';
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -6,12 +7,17 @@ const Shop = () => {
     useEffect(() =>{
         fetch('products.json')
         .then(res => res.json())
-        .then(data => console.log(data));
-    }, [])
+        .then(data => setProducts(data));
+    }, []);
     return (
         <div className='shop-container'>
             <div className="products-container">
-                <h2>hjsbfjsbn</h2>
+                {
+                    products.map(product=><Product 
+                        key={product.id}
+                        product={product}
+                        ></Product>)
+                }
             </div>
             <div className="cart-container">
                 <h3>Selected Clothes</h3>
