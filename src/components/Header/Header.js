b5ed6f70128b1d927a6f87b5c3d2fcd4be4621ac
Fahim Rimon <fahimrimon21@gmail.com>
2022-03-25 11:55:47 -0700

    header and data load

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <div className='text-center mt-3'>
+            <h1 className='text-warning'>Book House</h1>
+            <h3>Choose any 4 Book!</h3>
+        </div>
+    );
+};
+
+export default Header;
