b10e4e3b0ffdfffa259ac546e100ee1b27816315
Fahim Rimon <fahimrimon21@gmail.com>
2022-03-25 14:29:29 -0700

    product side complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.css';
 const Header = () => {
     return (
-        <div className='text-center mt-3'>
+        <div className='text-center mt-4'>
             <h1 className='main-name'>Book House</h1>
             <h3>Choose any 4 Book!</h3>
         </div>
