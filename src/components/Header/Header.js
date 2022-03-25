0ef27fe7aa92eb956f5154ef8fcd6a50f67aa7c4
Fahim Rimon <fahimrimon21@gmail.com>
2022-03-25 14:07:02 -0700

    btn handling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 const Header = () => {
     return (
         <div className='text-center mt-3'>
-            <h1 className='text-warning'>Book House</h1>
+            <h1 className='main-name'>Book House</h1>
             <h3>Choose any 4 Book!</h3>
         </div>
     );
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Product.css';
 const Product = (props) => {
+    const {handleAddToCart}= props;
  const {img, name, price}= props.product;
+ 
     return (
         <div className='product'>
             <img src={img} alt="" />
+            <div className="product-info">
+            <p className='product-name'>{name}</p>
+            <p>Price: ${price}</p>
+            </div>
+            <button onClick={()=> handleAddToCart(props.product)} className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
